08862e64f195884cf5937fbcd830f2f42ca235bb
wishu <wish@sek.ai>
2018-12-03 21:06:00 +1100

    :\

--- 1/part2.py
+++ 1/part2.py
@@ -1,15 +1,13 @@
 frequencies = open("input", "r").read().strip().split("\n")
 total_frequency = 0
-previous_freqs = []
-dupe_found = False
-while dupe_found is False:
+previous_freqs = {0}
+while True:
     for frequency in frequencies:
         if frequency.startswith("+"):
             total_frequency += int(frequency[1:])
         else:
             total_frequency -= int(frequency[1:])
         if total_frequency in previous_freqs:
-            print("Found duplicate", total_frequency)
-            dupe_found = True
-            break
-        previous_freqs.append(total_frequency)
+            print(f"Duplicate frequency: {total_frequency}")
+            exit()
+        previous_freqs.add(total_frequency)
