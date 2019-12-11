73c39795006c70c5e84f23231a23cb490996dfc2
wish <wish@sek.ai>
2019-12-11 19:37:19 +1100

    holy HOLY dude that was something

--- 2019/3/part2.py
+++ 2019/3/part2.py
@@ -1,47 +1,33 @@
 a = open("input","r").read().strip().split("\n")
 a[0] = a[0].split(",")
 a[1] = a[1].split(",")
-b, c, d = [], [], []
-cx, cy = 0, 0
-for x in a[0]:
-    for y in range(0, int(x[1:])):
-        if x[:1] == "R":
-            b.append(f"{cx+y},{cy}")
-        elif x[:1] == "U":
-            b.append(f"{cx},{cy+y}")
-        elif x[:1] == "L":
-            b.append(f"{cx-y},{cy}")
-        elif x[:1] == "D":
-            b.append(f"{cx},{cy-y}")
-    if x[:1] == "R":
-        cx += int(x[1:])
-    elif x[:1] == "U":
-        cy += int(x[1:])
-    elif x[:1] == "L":
-        cx -= int(x[1:])
-    elif x[:1] == "D":
-        cy -= int(x[1:])
-cx = 0
-cy = 0
-for x in a[1]:
-    for y in range(0, int(x[1:])):
-        if x[:1] == "R":
-            c.append(f"{cx+y},{cy}")
-        elif x[:1] == "U":
-            c.append(f"{cx},{cy+y}")
-        elif x[:1] == "L":
-            c.append(f"{cx-y},{cy}")
-        elif x[:1] == "D":
-            c.append(f"{cx},{cy-y}")
-    if x[:1] == "R":
-        cx += int(x[1:])
-    elif x[:1] == "U":
-        cy += int(x[1:])
-    elif x[:1] == "L":
-        cx -= int(x[1:])
-    elif x[:1] == "D":
-        cy -= int(x[1:])
-for x in range(0, len(b)):
-    if b[x] in c:
-        d.append(b.index(b[x]) + c.index(b[x]))
-print(min(d[1:]))
+b = [{},{}]
+c = [set(),set()]
+d = []
+for x in range(0, len(a)):
+    cx = 0
+    cy = 0
+    dis = 0
+    for y in a[x]:
+        for z in range(0, int(y[1:])):
+            if y[:1] == "R":
+                b[x][dis] = f"{cx+z},{cy}"
+            elif y[:1] == "U":
+                b[x][dis] = f"{cx},{cy+z}"
+            elif y[:1] == "L":
+                b[x][dis] = f"{cx-z},{cy}"
+            elif y[:1] == "D":
+                b[x][dis] = f"{cx},{cy-z}"
+            dis += 1
+        if y[:1] == "R":
+            cx += int(y[1:])
+        elif y[:1] == "U":
+            cy += int(y[1:])
+        elif y[:1] == "L":
+            cx -= int(y[1:])
+        elif y[:1] == "D":
+            cy -= int(y[1:])
+c = set(b[0].values()).intersection(set(b[1].values()))
+for x in c:
+    d.append(list(b[0].keys())[list(b[0].values()).index(x)] + list(b[1].keys())[list(b[1].values()).index(x)])
+print(min(i for i in d if i > 0))
