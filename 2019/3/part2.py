1ebaf20cc9d9dae9b9694c20c7e411a984aa9d9d
wish <wish@sek.ai>
2019-12-11 19:50:42 +1100

    might as well COMMIT to this set thing

--- 2019/3/part2.py
+++ 2019/3/part2.py
@@ -3,7 +3,7 @@ a[0] = a[0].split(",")
 a[1] = a[1].split(",")
 b = [{},{}]
 c = [set(),set()]
-d = []
+d = set()
 for x in range(0, len(a)):
     cx = 0
     cy = 0
@@ -29,5 +29,5 @@ for x in range(0, len(a)):
             cy -= int(y[1:])
 c = set(b[0].values()).intersection(set(b[1].values()))
 for x in c:
-    d.append(list(b[0].keys())[list(b[0].values()).index(x)] + list(b[1].keys())[list(b[1].values()).index(x)])
+    d.add(list(b[0].keys())[list(b[0].values()).index(x)] + list(b[1].keys())[list(b[1].values()).index(x)])
 print(min(i for i in d if i > 0))
