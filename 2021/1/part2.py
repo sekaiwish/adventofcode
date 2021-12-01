2e4a04f2f0f30f3338cb672d119d2cd5dc2ecd5a
wishu <wish@sek.ai>
2021-12-01 17:57:54 +1100

    optimise 2021 d1p2

--- 2021/1/part2.py
+++ 2021/1/part2.py
@@ -2,12 +2,11 @@ with open('input', 'r') as fp:
     data = [int(line.rstrip()) for line in fp]
 increased = 0
 i = 0
+previous = data[0] + data[1] + data[2]
 for _ in data[:-2]:
     data[i] += data[i+1] + data[i+2]
-    i += 1
-previous = data[0]
-for line in data[:-2]:
-    if line > previous:
+    if data[i] > previous:
         increased += 1
-    previous = line
+    previous = data[i]
+    i += 1
 print(increased)
